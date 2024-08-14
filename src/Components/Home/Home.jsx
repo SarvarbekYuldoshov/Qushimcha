c50ae63c4d9911c4c10dd64e69cfdd9c3f15a222
Sarvarbek Yuldoshov <sarvarbekyuldoshov85@gmail.com>
2024-08-14 20:12:55 +0300

    salom

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,8 +1,21 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import "./Home.css"
+import axios from 'axios'
 const Home = () => {
-    const [cities,setCities] = useState('')
-    
+    const [cities,setCities] = useState([])
+    const getCities = () =>{
+        axios.get('https://autoapi.dezinfeksiyatashkent.uz/api/cities')
+        .then(res=>setCities(res.data.data))
+        .catch(err=>console.log(err))
+    }
+    useEffect(()=>{
+        getCities()
+    },[])
+    const columns = [
+        {
+            title:"Name"
+        }
+    ]
   return (
     <div>
       
