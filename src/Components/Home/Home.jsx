9f1340a693ed8cb53b530ff22b52859ac1a58575
Sarvarbek Yuldoshov <sarvarbekyuldoshov85@gmail.com>
2024-08-14 23:16:30 +0300

    salom

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import "./Home.css"
 import axios from 'axios'
-import { Table } from 'antd'
+import { Button, Table } from 'antd'
 const Home = () => {
     const [cities,setCities] = useState([])
     const getCities = () =>{
@@ -28,6 +28,10 @@ const Home = () => {
         {
             title:"Images",
             dataIndex:"images",
+        },
+        {
+            title:"Action",
+            dataIndex:"action",
         }
     ]
     const data = cities.map((city,index)=>(
@@ -36,12 +40,17 @@ const Home = () => {
             number:index+1,
             name:city.name,
             text:city.text,
-            images:<img width={150} src={`https://autoapi.dezinfeksiyatashkent.uz/api/uploads/images/${city.image_src}`}/>
+            images:
+            <img 
+            width={150} 
+            src={`https://autoapi.dezinfeksiyatashkent.uz/api/uploads/images/${city.image_src}`}/>,
+            action:<><Button className='home-btn-a'>Edit</Button><Button className='home-btn-b'>Delete</Button></>
         }
     ))
   return (
     <div className='home'>
         <div className='container home-container'>
+            <Button className='home-btn'>Shahar Qushish</Button>
             <Table columns={columns} dataSource={data}/>
         </div>
     </div>
