455845a67f1d2066f9fc4427fb1a64fb9c495752
Sarvarbek Yuldoshov <sarvarbekyuldoshov85@gmail.com>
2024-08-17 01:00:45 +0300

    salom

--- src/Components/Brands/Brands.jsx
+++ src/Components/Brands/Brands.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./Brands.css"
+import axios from 'axios';
 const Brands = () => {
+    const[brands,setBrands] = useState([]);
+    const getBrands = () => {
+        axios.get('https://autoapi.dezinfeksiyatashkent.uz/api/brands')
+        .then((res)=>setBrands(res.data.data))
+        .catch((err)=>console.log(err))
+    }
+    useEffect(()=>{
+        getBrands();
+    })
+    const columns = [
+        {
+            title: 'Brand',
+            dataIndex: 'brand_name',
+        },
+        {
+            title: 'Logo',
+            dataIndex: 'brand_logo',
+        },
+        {
+            title: 'Action',
+            key: 'action',
+        },
+    ]
   return (
     <div className='brands'>
       <div className='container brands-container'>
-        
+
       </div>
     </div>
   )
