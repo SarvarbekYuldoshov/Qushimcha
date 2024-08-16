fddb2f457cf5634ed87d46957d237415ddf0a3d6
Sarvarbek Yuldoshov <sarvarbekyuldoshov85@gmail.com>
2024-08-16 22:29:46 +0300

    salom

--- src/Components/Models/Models.jsx
+++ src/Components/Models/Models.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import "./Models.css"
+import axios from 'axios'
 const Models = () => {
+    const [models, setModels] = React.useState([])
+    const [loading, setLoading] = React.useState(true)
+    const getModels = () =>{
+        axios.get('https://autoapi.dezinfeksiyatashkent.uz/api/models')
+        .then(res=>setModels(res.data.data))
+        .catch(err=>console.log(err))
+    }
+    useEffect(()=>{
+        getModels()
+    },[])
   return (
-    <div>
-      Models
+    <div className='model'>
+      <div className='container model-container'>
+        {
+            <ul className='model-list'>
+                <h1 className='model-title'>Model</h1>
+                <h2 className='model-title'>Brand</h2>
+            </ul>
+        }
+         {
+            models && models.map((item,index)=>(
+                <div key={index}>
+                   
+                </div>
+            ))
+         }
+      </div>
     </div>
   )
 }
